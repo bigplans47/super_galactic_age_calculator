430988c8a9e4d5259d1975e43156fe1f257cdbdd
Dan Kiss <flowfast47@gmail.com>
2017-10-13 16:08:20 -0700

    set up a passing spec

---
+++ spec/calculator-spec.js
@@ -0,0 +1,40 @@
+import { Calculator } from './../js/calculator.js';
+var sample = new Calculator('22');
+
+describe('Calculator', function () {
+  it('should have an age', function () {
+    expect(sample.age).toEqual('22');
+  });
+
+});
+
+// describe('Band', function () {
+//   it('should have a name', function () {
+//     expect(bandito.name).toEqual("Bandito");
+//   });
+
+  // it('characters to equal empty array', function() {
+  //   expect(bandito.characters).toEqual([]);
+  // });
+  //
+  // it('should add a character to a band', function() {
+  //   bandito.addCharacters("Lilly");
+  //   expect(bandito.characters).toEqual(['Lilly']);
+  // });
+  //
+  // it('should return a number when band rolls', function() {
+  //   expect(bandito.roll()).toEqual(jasmine.any(Number));
+  // });
+  //
+  // it('should alert and set fans to zero when a 1 is rolled', function() {
+  //   bandito.level = 1;
+  //   bandito.roll();
+  //   expect(bandito.fans).toEqual(0);
+  // });
+  //
+  // it('should add a groupie for every 10 fans', function() {
+  //   bandito.fans = 20;
+  //   bandito.groupie();
+  //   console.log(bandito.characters);
+  //   expect(bandito.characters).toEqual(['Lilly',"groupie"]);
+  // });
